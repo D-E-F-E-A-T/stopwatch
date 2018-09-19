dbfba5777f3bf9cfad8acab4f96d01a2b5e76773
Tobiasz18 <tobiaszpost@gmail.com>
2018-09-19 17:41:39 +0200

    create elements

--- dist/stopwatch.9d477285.js
+++ dist/stopwatch.9d477285.js
@@ -104,182 +104,53 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   // Override the current require with this new one
   return newRequire;
 })({"stopwatch.js":[function(require,module,exports) {
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var elem = { a: 'alba', b: 'beata' };
-var x = _extends({
-    c: "orakle"
-}, elem);
-console.log(x);
-},{}],"..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
-var global = arguments[3];
-var OVERLAY_ID = '__parcel__error__overlay__';
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var OldModule = module.bundle.Module;
+var Stopwatch = function () {
+    function Stopwatch() {
+        _classCallCheck(this, Stopwatch);
 
-function Module(moduleName) {
-  OldModule.call(this, moduleName);
-  this.hot = {
-    data: module.bundle.hotData,
-    _acceptCallbacks: [],
-    _disposeCallbacks: [],
-    accept: function (fn) {
-      this._acceptCallbacks.push(fn || function () {});
-    },
-    dispose: function (fn) {
-      this._disposeCallbacks.push(fn);
+        this.elem = this.display();
+        this.createButtons();
     }
-  };
 
-  module.bundle.hotData = null;
-}
-
-module.bundle.Module = Module;
-
-var parent = module.bundle.parent;
-if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
-  var hostname = '' || location.hostname;
-  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54660' + '/');
-  ws.onmessage = function (event) {
-    var data = JSON.parse(event.data);
-
-    if (data.type === 'update') {
-      console.clear();
-
-      data.assets.forEach(function (asset) {
-        hmrApply(global.parcelRequire, asset);
-      });
-
-      data.assets.forEach(function (asset) {
-        if (!asset.isNew) {
-          hmrAccept(global.parcelRequire, asset.id);
+    _createClass(Stopwatch, [{
+        key: 'createButtons',
+        value: function createButtons() {
+            //we will refer to elements by nodes e.g this.elem.children[0]
+            var buttonStart = document.createElement('button');
+            var buttonStop = document.createElement('button');
+            var buttonLop = document.createElement('button');
+            var buttonClear = document.createElement('button');
+            var buttonClearLop = document.createElement('button');
+            buttonStart.setAttribute('id', 'btn_start');
+            buttonStop.setAttribute('id', 'btn_stop');
+            buttonLop.setAttribute('id', 'btn_lop');
+            buttonClear.setAttribute('id', 'btn_clear');
+            buttonClearLop.setAttribute('id', 'btn_clearLop');
+            this.elem.appendChild(buttonStart);
+            this.elem.appendChild(buttonStop);
+            this.elem.appendChild(buttonLop);
+            this.elem.appendChild(buttonClear);
+            this.elem.appendChild(buttonClearLop);
         }
-      });
-    }
+    }, {
+        key: 'display',
+        value: function display() {
+            var container = document.createElement('div');
+            container.setAttribute('id', 'container_id');
 
-    if (data.type === 'reload') {
-      ws.close();
-      ws.onclose = function () {
-        location.reload();
-      };
-    }
+            return container;
+        }
+    }]);
 
-    if (data.type === 'error-resolved') {
-      console.log('[parcel] ✨ Error resolved');
+    return Stopwatch;
+}();
 
-      removeErrorOverlay();
-    }
-
-    if (data.type === 'error') {
-      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
-
-      removeErrorOverlay();
-
-      var overlay = createErrorOverlay(data);
-      document.body.appendChild(overlay);
-    }
-  };
-}
-
-function removeErrorOverlay() {
-  var overlay = document.getElementById(OVERLAY_ID);
-  if (overlay) {
-    overlay.remove();
-  }
-}
-
-function createErrorOverlay(data) {
-  var overlay = document.createElement('div');
-  overlay.id = OVERLAY_ID;
-
-  // html encode message and stack trace
-  var message = document.createElement('div');
-  var stackTrace = document.createElement('pre');
-  message.innerText = data.error.message;
-  stackTrace.innerText = data.error.stack;
-
-  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
-
-  return overlay;
-}
-
-function getParents(bundle, id) {
-  var modules = bundle.modules;
-  if (!modules) {
-    return [];
-  }
-
-  var parents = [];
-  var k, d, dep;
-
-  for (k in modules) {
-    for (d in modules[k][1]) {
-      dep = modules[k][1][d];
-      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
-        parents.push(k);
-      }
-    }
-  }
-
-  if (bundle.parent) {
-    parents = parents.concat(getParents(bundle.parent, id));
-  }
-
-  return parents;
-}
-
-function hmrApply(bundle, asset) {
-  var modules = bundle.modules;
-  if (!modules) {
-    return;
-  }
-
-  if (modules[asset.id] || !bundle.parent) {
-    var fn = new Function('require', 'module', 'exports', asset.generated.js);
-    asset.isNew = !modules[asset.id];
-    modules[asset.id] = [fn, asset.deps];
-  } else if (bundle.parent) {
-    hmrApply(bundle.parent, asset);
-  }
-}
-
-function hmrAccept(bundle, id) {
-  var modules = bundle.modules;
-  if (!modules) {
-    return;
-  }
-
-  if (!modules[id] && bundle.parent) {
-    return hmrAccept(bundle.parent, id);
-  }
-
-  var cached = bundle.cache[id];
-  bundle.hotData = {};
-  if (cached) {
-    cached.hot.data = bundle.hotData;
-  }
-
-  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
-    cached.hot._disposeCallbacks.forEach(function (cb) {
-      cb(bundle.hotData);
-    });
-  }
-
-  delete bundle.cache[id];
-  bundle(id);
-
-  cached = bundle.cache[id];
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    cached.hot._acceptCallbacks.forEach(function (cb) {
-      cb();
-    });
-    return true;
-  }
-
-  return getParents(global.parcelRequire, id).some(function (id) {
-    return hmrAccept(global.parcelRequire, id);
-  });
-}
-},{}]},{},["..\\..\\AppData\\Roaming\\npm\\node_modules\\parcel-bundler\\src\\builtins\\hmr-runtime.js","stopwatch.js"], null)
+var stopwatch = new Stopwatch();
+document.body.appendChild(stopwatch.elem);
+console.log(stopwatch.elem.children[0]);
+},{}]},{},["stopwatch.js"], null)
 //# sourceMappingURL=/stopwatch.9d477285.map
--- stopwatch.js
+++ stopwatch.js
@@ -1,12 +1,36 @@
 class Stopwatch {
     constructor() {
-        this.elem = this.display; 
+        this.elem = this.display();
+        this.createButtons();
+    }
+
+    createButtons() { //we will refer to elements by nodes e.g this.elem.children[0]
+        const buttonStart = document.createElement('button');
+        const buttonStop = document.createElement('button');
+        const buttonLop = document.createElement('button');
+        const buttonClear = document.createElement('button');
+        const buttonClearLop = document.createElement('button');
+        buttonStart.setAttribute('id', 'btn_start');
+        buttonStop.setAttribute('id', 'btn_stop');
+        buttonLop.setAttribute('id', 'btn_lop');
+        buttonClear.setAttribute('id', 'btn_clear');
+        buttonClearLop.setAttribute('id', 'btn_clearLop');
+        this.elem.appendChild(buttonStart);
+        this.elem.appendChild(buttonStop);
+        this.elem.appendChild(buttonLop);
+        this.elem.appendChild(buttonClear);
+        this.elem.appendChild(buttonClearLop);
     }
 
     display() {
         const container = document.createElement('div');
-        contaier.setAttribute('id', 'container_id');
+        container.setAttribute('id', 'container_id');
 
         return container
     }
 }
+
+
+const stopwatch = new Stopwatch();
+document.body.appendChild(stopwatch.elem);
+console.log(stopwatch.elem.children[0])
